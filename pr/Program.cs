5bb8979f3b993464093ac5892a37f128b17eac87
Адылканов Муратжан Талгатович <@corp.itstep>
2019-03-24 18:59:51 +0600

    Практическая работа

---
+++ pr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите от какого числа начинается ваша подстрока: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите размер подстроки: ");
+            int finish = int.Parse(Console.ReadLine());
+            char[] name = new char[100];
+            Console.WriteLine("Введите имя: ");
+            String text = Console.ReadLine();
+            name = text.ToCharArray();
+            Type type = text.GetType();
+            object valid = Activator.CreateInstance(type, new Object[] { name, start, finish });
+            Console.WriteLine(valid);
+            Console.Read();
+        }
+    }
+}
---
+++ pr2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace pr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Type type = typeof(List<>);
+            ConstructorInfo[] constructors = type.GetConstructors();
+            Console.WriteLine("Конструкторы List<>: ");
+            foreach(var construct in constructors)
+            {
+                Console.WriteLine(construct.ToString());
+            }
+            Console.Read();
+        }
+    }
+}
